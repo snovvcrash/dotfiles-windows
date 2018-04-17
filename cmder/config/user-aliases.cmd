27842230a78dd74afe75f96fc9311b34747ca499
Sam Frees1de <snovvcrash@protonmail.com>
2018-04-17 23:12:19 +0300

    Initial commit

---
+++ cmder/config/user-aliases.cmd
@@ -0,0 +1,22 @@
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+
+;= rem Custom settings
+l=ls -lah --color $*
+;= rem python=winpty python.exe
+mkvirtualenv=mkvirtualenv.bat
+rmvirtualenv=rmvirtualenv.bat
+workon=workon.bat
